6b347d87ca5fb75de3fe93499179af64f6ceb6ac
Harshit <harshitlarenc@gmail.com>
2026-07-21 19:45:06 +0530

    Testing

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = Number(process.env.PORT) || 3000;
-export const INVITE_URL_BASE = process.env.INVITE_URL_BASE || 'http://localhost:3000/join/';
+export const INVITE_URL_BASE = process.env.INVITE_URL_BASE || '';
 export const MAX_CHAT_LENGTH = 120;
 export const MAX_PACKETS_PER_SEC = 15;
 export const THROTTLE_COOLDOWN_MS = 5000; // Time user gets blocked if packet limit exceeded
