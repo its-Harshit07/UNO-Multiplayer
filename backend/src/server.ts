24cf13e08cbf3db8294ccd67b1663246b344f9bf
Harshit <harshitlarenc@gmail.com>
2026-07-22 17:09:25 +0530

    Bug fixes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,7 @@ app.get('/health', (req, res) => {
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: 'https://playuno-hl.vercel.app/',
+    origin: 'playuno-hl.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
